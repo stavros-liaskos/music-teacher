7d717ccb67f61232fdd142b735862313099391a1
Stalias <st.liaskos@gmail.com>
2023-04-30 18:56:20 +0200

    feat(content): improve content

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -6,7 +6,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   // Put Header or Footer Here
   return (
     <>
-      <div className='mx-4 mb-40 mt-8 flex max-w-4xl flex-col antialiased md:mt-20 md:flex-row lg:mx-auto lg:mt-32'>
+      <div className='mx-4 mb-40 mt-8 flex max-w-4xl flex-col antialiased md:mt-20 md:flex-row md:gap-10 lg:mx-auto lg:mt-32'>
         <Sidebar />
         <main className='mt-6 flex min-w-0 flex-auto flex-col px-2 md:mt-0 md:px-0'>
           {children}
